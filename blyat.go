638d65f2e4f0b23a7b530da398a5107a74bf0dda
Kernux <alexeipolovin@gmail.com>
2019-06-10 18:49:11 +0300

    Fuck

--- blyat.go
+++ blyat.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -17,22 +16,26 @@ type kek struct {
 }
 
 func Kick(chatid int64, userid int) {
-	//bot, err := tgbotapi.NewBotAPI("669872325:AAFU0Fn6QHXnoU12LYi7CxxXem2GF8eemDA")
-	token := "669872325:AAFU0Fn6QHXnoU12LYi7CxxXem2GF8eemDA"
-	/*if err != nil {
-		log.Panic(err)
-	}
-	t := time.Date(2001, time.September, 9, 1, 46, 40, 0, time.UTC)
-	k := tgbotapi.ChatMemberConfig{
-		ChatID: chatid,
-		UserID: userid,
-	}
-	bot.KickChatMember(tgbotapi.KickChatMemberConfig{k, t.Unix()})
-	*/
+	var token string = "669872325:AAFU0Fn6QHXnoU12LYi7CxxXem2GF8eemDA"
 	usid := string(userid)
 	http.Get("https://api.telegram.org/bot" + token + "/kickChatMember?chat_id=@grobkernux&user_id=" + usid)
 }
-
+func adminCheck(_chid *int64, _uid *int, _token *string) {
+	apiGet, err := http.Get("https://api.telegram.org/bot" + *_token + "/getChatMember?chat_id=@grobkernux&user_id" + string(*_uid))
+	if err != nil {
+		log.Println(err)
+	}
+	var app = kek{}
+	//var api map[string]interface{}
+	json.NewDecoder(apiGet.Body).Decode(&app)
+	switch string(app.result.status) {
+	case "admin":
+		Kick(*_chid, *_uid)
+	case "creator":
+		Kick(*_chid, *_uid)
+	default:
+	}
+}
 func main() {
 	bot, err := tgbotapi.NewBotAPI("669872325:AAFU0Fn6QHXnoU12LYi7CxxXem2GF8eemDA")
 	token := "669872325:AAFU0Fn6QHXnoU12LYi7CxxXem2GF8eemDA"
@@ -56,22 +59,16 @@ func main() {
 			case "help":
 				msg.Text = "try /ban and /f"
 			case "ban":
-				t := time.Date(2001, time.September, 9, 1, 46, 40, 0, time.UTC)
-				k := tgbotapi.ChatMemberConfig{
-					ChatID: update.Message.Chat.ID,
-					UserID: update.Message.ReplyToMessage.From.ID,
-				}
 				chid := update.Message.Chat.ID
 				uid := update.Message.From.ID
-				uid_string := string(uid)
-				bot.KickChatMember(tgbotapi.KickChatMemberConfig{k, t.Unix()})
-				api_get, err := http.Get("https://api.telegram.org/bot" + token + "/getChatMember?chat_id=@grobkernux&user_id" + uid_string)
+				uidString := string(uid)
+				apiGet, err := http.Get("https://api.telegram.org/bot" + token + "/getChatMember?chat_id=@grobkernux&user_id" + uidString)
 				if err != nil {
 					log.Println(err)
 				}
 				var app = kek{}
 				//var api map[string]interface{}
-				json.NewDecoder(api_get.Body).Decode(&app)
+				json.NewDecoder(apiGet.Body).Decode(&app)
 				switch string(app.result.status) {
 				case "admin":
 					Kick(chid, uid)
@@ -82,11 +79,10 @@ func main() {
 				}
 
 			case "stable":
-			//	uid := update.Message.From.ID
-			//	roll := tgbotapi.ChatMember{tgbotapi.User{update.Message.From.ID, update.Message.From.FirstName}, update.Message.From.ID}.IsAdministrator()
+
 			case "dev":
 			default:
-				msg.Text = "I don't know that command"
+
 			}
 			bot.Send(msg)
 		}
