1bbd04c655870d58af0eef214ba8f21ece2337e3
Kernux <alexeipolovin@gmail.com>
2019-06-11 00:33:53 +0300

    I'm good

--- admincheck.go
+++ admincheck.go
@@ -23,7 +23,7 @@ func checkAdmin(_strchatID *string, _struserID *string, _chatID *int64, _userID 
 	case "administrator":
 		kick(*_replyID, *_chatID)
 	case "creator":
-		kick(*_userID, *_chatID)
+		kick(*_replyID, *_chatID)
 	default:
 
 	}
