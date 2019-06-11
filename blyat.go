a4c7c94cf1e4c1d6883d8e4b9f70519589e2696b
Kernux <alexeipolovin@gmail.com>
2019-06-11 16:55:05 +0300

    Unsplah

--- blyat.go
+++ blyat.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"math/rand"
 	"net/http"
@@ -10,6 +11,77 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+type unsplash struct {
+	ID             string      `json:"id"`
+	CreatedAt      string      `json:"created_at"`
+	UpdatedAt      string      `json:"updated_at"`
+	Width          int         `json:"width"`
+	Height         int         `json:"height"`
+	Color          string      `json:"color"`
+	Description    interface{} `json:"description"`
+	AltDescription interface{} `json:"alt_description"`
+	Urls           struct {
+		Raw     string `json:"raw"`
+		Full    string `json:"full"`
+		Regular string `json:"regular"`
+		Small   string `json:"small"`
+		Thumb   string `json:"thumb"`
+	} `json:"urls"`
+	Links struct {
+		Self             string `json:"self"`
+		HTML             string `json:"html"`
+		Download         string `json:"download"`
+		DownloadLocation string `json:"download_location"`
+	} `json:"links"`
+	Categories             []interface{} `json:"categories"`
+	Sponsored              bool          `json:"sponsored"`
+	SponsoredBy            interface{}   `json:"sponsored_by"`
+	SponsoredImpressionsID interface{}   `json:"sponsored_impressions_id"`
+	Likes                  int           `json:"likes"`
+	LikedByUser            bool          `json:"liked_by_user"`
+	CurrentUserCollections []interface{} `json:"current_user_collections"`
+	User                   struct {
+		ID              string      `json:"id"`
+		UpdatedAt       string      `json:"updated_at"`
+		Username        string      `json:"username"`
+		Name            string      `json:"name"`
+		FirstName       string      `json:"first_name"`
+		LastName        string      `json:"last_name"`
+		TwitterUsername interface{} `json:"twitter_username"`
+		PortfolioURL    interface{} `json:"portfolio_url"`
+		Bio             string      `json:"bio"`
+		Location        string      `json:"location"`
+		Links           struct {
+			Self      string `json:"self"`
+			HTML      string `json:"html"`
+			Photos    string `json:"photos"`
+			Likes     string `json:"likes"`
+			Portfolio string `json:"portfolio"`
+			Following string `json:"following"`
+			Followers string `json:"followers"`
+		} `json:"links"`
+		ProfileImage struct {
+			Small  string `json:"small"`
+			Medium string `json:"medium"`
+			Large  string `json:"large"`
+		} `json:"profile_image"`
+		InstagramUsername string `json:"instagram_username"`
+		TotalCollections  int    `json:"total_collections"`
+		TotalLikes        int    `json:"total_likes"`
+		TotalPhotos       int    `json:"total_photos"`
+		AcceptedTos       bool   `json:"accepted_tos"`
+	} `json:"user"`
+	Exif struct {
+		Make         string `json:"make"`
+		Model        string `json:"model"`
+		ExposureTime string `json:"exposure_time"`
+		Aperture     string `json:"aperture"`
+		FocalLength  string `json:"focal_length"`
+		Iso          int    `json:"iso"`
+	} `json:"exif"`
+	Views     int `json:"views"`
+	Downloads int `json:"downloads"`
+}
 type kek struct {
 	Ok     bool `json:"ok"`
 	Result struct {
@@ -25,6 +97,7 @@ type kek struct {
 }
 
 func main() {
+	var text string
 	var (
 		gayRand int
 		counter int
@@ -91,7 +164,7 @@ func main() {
 				msg.Text = "You are gay with chance:" + strconv.Itoa(gayRand) + "%"
 				counter++
 			case "8":
-				gayRand = rand.Intn(3)
+				gayRand = rand.Intn(4)
 				switch gayRand {
 				case 0:
 					msg.Text = "Мой ответ - 'да'"
@@ -102,6 +175,17 @@ func main() {
 				case 3:
 					msg.Text = "Мой ответ-'нет'"
 				}
+			case "unsplash":
+				httpGet, err := http.Get("https://api.unsplash.com/photos/random?client_id=1435c8eaadfbeacd502ec854e73123059456f3a601722e790c009bd40fdfe15b")
+				if err != nil {
+					log.Print(err)
+				}
+				var photos = unsplash{}
+				json.NewDecoder(httpGet.Body).Decode(&photos)
+				text = photos.Links.Download
+				msg.Text = text
+			case "shrug":
+				msg.Text = "¯\\_(ツ)_/¯'"
 			case "Foxed":
 				msg.Text = "http://qiwi.me/f0x1d"
 			case "stable":
