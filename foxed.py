5b239c595aa25deacb01aafc9402297da7efe345
GrOb <alexeipolovin@gmail.com>
2019-06-11 16:29:37 +0000

    foxed.py edited online with Bitbucket

--- foxed.py
+++
@@ -1,5 +0,0 @@
-import telebot
-bot = telebot.TeleBot("845099949:AAHGgsnVDhr0KUqIUW4ilHNXvcN9vXv3hfE")
-@bot.message_handler(commands=['start','help'])
-def send_flex(message):
-	bot.reply_to(message, "Flex")
