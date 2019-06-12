999f31c2e2d19c467d1d840009c6ef89160d69ff
Kernux <alexeipolovin@gmail.com>
2019-06-12 22:19:13 +0300

    Music

--- blyat.go
+++ blyat.go
@@ -11,10 +11,91 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-type nilcheck struct {
-	replyIDNil int
+type music struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Upc         string `json:"upc"`
+	Link        string `json:"link"`
+	Share       string `json:"share"`
+	Cover       string `json:"cover"`
+	CoverSmall  string `json:"cover_small"`
+	CoverMedium string `json:"cover_medium"`
+	CoverBig    string `json:"cover_big"`
+	CoverXl     string `json:"cover_xl"`
+	GenreID     int    `json:"genre_id"`
+	Genres      struct {
+		Data []struct {
+			ID      int    `json:"id"`
+			Name    string `json:"name"`
+			Picture string `json:"picture"`
+			Type    string `json:"type"`
+		} `json:"data"`
+	} `json:"genres"`
+	Label                 string `json:"label"`
+	NbTracks              int    `json:"nb_tracks"`
+	Duration              int    `json:"duration"`
+	Fans                  int    `json:"fans"`
+	Rating                int    `json:"rating"`
+	ReleaseDate           string `json:"release_date"`
+	RecordType            string `json:"record_type"`
+	Available             bool   `json:"available"`
+	Tracklist             string `json:"tracklist"`
+	ExplicitLyrics        bool   `json:"explicit_lyrics"`
+	ExplicitContentLyrics int    `json:"explicit_content_lyrics"`
+	ExplicitContentCover  int    `json:"explicit_content_cover"`
+	Contributors          []struct {
+		ID            int    `json:"id"`
+		Name          string `json:"name"`
+		Link          string `json:"link"`
+		Share         string `json:"share"`
+		Picture       string `json:"picture"`
+		PictureSmall  string `json:"picture_small"`
+		PictureMedium string `json:"picture_medium"`
+		PictureBig    string `json:"picture_big"`
+		PictureXl     string `json:"picture_xl"`
+		Radio         bool   `json:"radio"`
+		Tracklist     string `json:"tracklist"`
+		Type          string `json:"type"`
+		Role          string `json:"role"`
+	} `json:"contributors"`
+	Artist struct {
+		ID            int    `json:"id"`
+		Name          string `json:"name"`
+		Picture       string `json:"picture"`
+		PictureSmall  string `json:"picture_small"`
+		PictureMedium string `json:"picture_medium"`
+		PictureBig    string `json:"picture_big"`
+		PictureXl     string `json:"picture_xl"`
+		Tracklist     string `json:"tracklist"`
+		Type          string `json:"type"`
+	} `json:"artist"`
+	Type   string `json:"type"`
+	Tracks struct {
+		Data []struct {
+			ID                    int    `json:"id"`
+			Readable              bool   `json:"readable"`
+			Title                 string `json:"title"`
+			TitleShort            string `json:"title_short"`
+			TitleVersion          string `json:"title_version"`
+			Link                  string `json:"link"`
+			Duration              int    `json:"duration"`
+			Rank                  int    `json:"rank"`
+			ExplicitLyrics        bool   `json:"explicit_lyrics"`
+			ExplicitContentLyrics int    `json:"explicit_content_lyrics"`
+			ExplicitContentCover  int    `json:"explicit_content_cover"`
+			Preview               string `json:"preview"`
+			Artist                struct {
+				ID        int    `json:"id"`
+				Name      string `json:"name"`
+				Tracklist string `json:"tracklist"`
+				Type      string `json:"type"`
+			} `json:"artist"`
+			Type string `json:"type"`
+		} `json:"data"`
+	} `json:"tracks"`
 }
-type News struct {
+
+type news struct {
 	Status       string `json:"status"`
 	TotalResults int    `json:"totalResults"`
 	Articles     []struct {
@@ -125,9 +206,6 @@ type kek struct {
 	} `json:"result"`
 }
 
-func nilek(replyIDPointer *int) {
-	return
-}
 func main() {
 	var (
 		unsplashResponse string = "https://api.unsplash.com/photos/random?client_id=1435c8eaadfbeacd502ec854e73123059456f3a601722e790c009bd40fdfe15b"
@@ -136,11 +214,11 @@ func main() {
 	)
 	var text string
 	var (
-		gayRand int
+		random  int
 		counter int
 	)
 	var (
-		replyID int = 0
+		replyID int
 		//strreplyID string
 	)
 	var (
@@ -202,18 +280,18 @@ func main() {
 				strchatID = strconv.FormatInt(chatID, 10)
 				http.Get("https://api.telegram.org/bot" + token + "/sendSticker?chat_id=" + strchatID + "&sticker=CAADAgADsgADTptkAm1WnTBWvUfiAg")
 			case "gay":
-				gayRand = rand.Intn(100)
+				random = rand.Intn(100)
 				if counter == 5 {
 					msg.Text = "Wait"
 					bot.Send(msg)
 					time.Sleep(10 * time.Second)
 					counter = 0
 				}
-				msg.Text = "You are gay with chance:" + strconv.Itoa(gayRand) + "%"
+				msg.Text = "You are gay with chance:" + strconv.Itoa(random) + "%"
 				counter++
 			case "8":
-				gayRand = rand.Intn(4)
-				switch gayRand {
+				random = rand.Intn(4)
+				switch random {
 				case 0:
 					msg.Text = "Мой ответ - 'да'"
 				case 1:
@@ -236,6 +314,14 @@ func main() {
 					text = photos.Links.Download
 					msg.Text = text
 				}
+			case "music":
+				random = rand.Intn(300000-100000) + 100000
+				var strrandom = strconv.Itoa(random)
+				httpGet, err := http.Get("https://api.deezer.com/album/" + strrandom)
+				errcheck(&err)
+				var mus = music{}
+				json.NewDecoder(httpGet.Body).Decode(&mus)
+				msg.Text = mus.Cover + mus.Link
 			case "flex":
 				chatID = update.Message.Chat.ID
 				strchatID = strconv.FormatInt(chatID, 10)
@@ -263,7 +349,7 @@ func main() {
 				rand := rand.Intn(7)
 				httpGet, err := http.Get(newsResponse)
 				errcheck(&err)
-				var news = News{}
+				var news = news{}
 				json.NewDecoder(httpGet.Body).Decode(&news)
 				URLText := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 				URLText.Text = news.Articles[rand].Title
